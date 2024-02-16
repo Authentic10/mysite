82082518dad2e91a12e42e5569e4990f4a219dbf
Ahmed Tchagnaou <ahmedtchagnaou@gmail.com>
2024-02-16 19:12:14 +0100

    Add the application url file

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import Http404
-
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+
 
 def post_list(request):
     posts = Post.published.all()
@@ -9,10 +8,7 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     
     return render(request, 'blog/post/detail.html', {'post': post})
 
